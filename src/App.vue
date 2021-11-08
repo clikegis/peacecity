d82c1f2e7cd067448b9b26a828815cb779567e72
cy <2449471714@qq.com>
2021-11-08 09:32:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div id="NoiseMapContainer">
+       <NoiseMap></NoiseMap>
+    </div>
+  </div>
+</template>
+
+<script>
+import NoiseMap from "@/components/NoiseMap";
+export default {
+  name: 'App',
+  components: {
+    NoiseMap
+  }
+}
+</script>
+
+<style>
+#app{
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+}
+/*地图容器*/
+#NoiseMapContainer{
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/components/NoiseMap.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="NoiseMap">
+      <div id="MapView">
+
+      </div>
+  </div>
+</template>
+
+<script>
+import {loadModules} from 'esri-loader'
+export default {
+  name: "NoiseMap",
+  mounted() {
+    this.createdMapView();
+  },
+  methods:{
+    createdMapView(){
+      const option = {
+        url: 'https://js.arcgis.com/4.21/',
+        css: "https://js.arcgis.com/4.21/esri/themes/dark/main.css"
+      };
+      loadModules(["esri/Map","esri/views/MapView","esri/layers/WMSLayer"],option).then(([Map,MapView,WMSLayer])=>{
+        let map = new Map({
+          basemap:'satellite'
+        });
+
+        let view = new MapView({ // eslint-disable-line no-unused-vars
+          map:map,
+          center:[-74.0,40.66],
+          zoom:12,
+          container:'MapView'
+        });
+
+
+      }).catch((err)=>{
+        console.log(err);
+      })
+    },
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+#NoiseMap{
+  height: 100%;
+  width: 100%;
+  position: relative;
+}
+#MapView{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+</style>
